0f14e4d4331cbb4f9ae846da65167c2e4283a933
Phoenixcoimbra <igormcoimbra@gmail.com>
2024-04-19 17:35:32 +0100

    quote api changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
+
+
 // Function to fetch a random quote from the Forismatic API
-function getRandomQuote() {
-    fetch('https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json')
-      .then(response => response.json())
-      .then(data => {
-        // Update UI with the retrieved quote and author
-        updateQuoteDisplay(data.quoteText, data.quoteAuthor);
-      })
-      .catch(error => console.error(error));
-  }
-  
+
+    function getRandomQuote() {
+        fetch('https://cors-anywhere.herokuapp.com/https://zenquotes.io/api/quotes/')
+          .then(response => response.json())
+          .then(data => {
+            updateQuoteDisplay(data.quoteText, data.quoteAuthor);
+          })
+          .catch(error => {
+            console.error('An error occurred:', error.message);
+            console.error(error.stack);
+          });
+
   // Function to update the UI elements with the fetched quote and author
   function updateQuoteDisplay(quoteText, quoteAuthor) {
     document.getElementById('quote-text').textContent = quoteText;
     if (quoteAuthor) { // Check if author information is available
-      document.getElementById('quote-author').textContent = '- ' + quoteAuthor;
+      document.getElementById('quote-author').textContent = '~ ' + quoteAuthor;
     } else {
       document.getElementById('quote-author').textContent = '';
     }
